961d3478c2c036d75921474b8ddfd37446a5d9f2
jiwon <jiwon_3261@naver.com>
2021-05-31 20:03:08 +0900

    front-mapping-시작

--- page/joinus/joinus.js
+++ page/joinus/joinus.js
@@ -1,39 +1,110 @@
 const registerMemberBtn = document.getElementById("registerMemberBtn");
+const addAddressBtn = document.getElementById("addAddressBtn");
+let inputAddressLocation = null;
 
-registerMemberBtn.addEventListener('click',()=>{
+addAddressBtn.addEventListener("click", () => {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      let config = {
+        headers: {
+          Authorization: "KakaoAK d40c82c42db0892f356fd71c6c36c7a0",
+        },
+      };
+      document.getElementById("address").value = data["jibunAddress"];
+      axios
+        .get(
+          "https://dapi.kakao.com/v2/local/search/address.json?query=" +
+            data["jibunAddress"],
+          config
+        )
+        .then((res) => {
+          const x = res["data"]["documents"][0]["address"]["x"];
+          const y = res["data"]["documents"][0]["address"]["y"];
+          inputAddressLocation = {
+            letitude: y,
+            longtitude: x,
+          };
+        });
+    },
+  }).open();
+});
+
+registerMemberBtn.addEventListener("click", () => {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
   const passwordChk = document.getElementById("passwordChk").value.trim();
 
-  if(InputValidator.isEmpty(email)){
-    alert('1');
-    return;
-  }
-  
-  if(InputValidator.isEmpty(password)){
-    alert('2');
+  if (InputValidator.isEmpty(email)) {
+    alert("1");
     return;
   }
 
-  if(InputValidator.isEmpty(passwordChk)){
-    alert('3');
-    return;
-  }
-  
-  if(!InputValidator.equal(password, passwordChk)){
-    alert('4');
+  if (InputValidator.isEmpty(password)) {
+    alert("2");
     return;
   }
 
-  axios.post('http://localhost:8081/api/v1/member',{
-    email : email,
-    password : password
-  },)
-    .then(function(response) {
-      alert('회원가입 완료');
-    })
-    .catch(function(error) {
-      alert(error.response['data']['msg']);
-    });
+  if (InputValidator.isEmpty(passwordChk)) {
+    alert("3");
+    return;
+  }
+
+  if (!InputValidator.equal(password, passwordChk)) {
+    alert(4);
+    return;
+  }
+
+  registerMember(email, password);
 });
 
+async function registerMember(email, password) {
+  //body양식
+  await axios
+    .post("http://10.202.36.60:8081/api/v1/member", {
+      //데이터인자
+      email: email,
+      password: password,
+    })
+    .then(function (response) {
+      alert("회원가입 완료");
+      accessToken(email, password);
+      addAddress(email);
+    })
+    .catch(function (error) {
+      alert(error.response["data"]["msg"]);
+    });
+}
+
+async function accessToken(email, password) {
+  const params = new URLSearchParams({
+    email: email,
+    password: password,
+  }).toString();
+  //쿼리
+  await axios
+    .post("http://10.202.36.60:8081/oauth/token?" + params)
+    .then(function (res) {
+      const accessToken = res["data"]["accessToken"];
+      const refreshToken = res["data"]["refreshToken"];
+
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+    })
+    .catch(function (res) {});
+}
+
+//header
+async function addAddress(email) {
+  if (inputAddressLocation != null) {
+    let config = {
+      headers: {
+        Authorization: localStorage.getItem("accessToken"),
+      },
+    };
+    await axios.put(
+      "http://10.202.36.60:8081/api/v1/member/address",
+      inputAddressLocation,
+      config
+    );
+  }
+}
--- page/main/main.js
+++ page/main/main.js
@@ -1,89 +1,123 @@
 window.addEventListener("DOMContentLoaded", () => {
-  var mapContainer = document.getElementById("store-map"), // 지도를 표시할 div
-    mapOption = {
-      center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
-      level: 4, // 지도의 확대 레벨
-    };
-
-  var map = new kakao.maps.Map(mapContainer, mapOption);
-
-  var imageSrc = "../../assets/img/marker-2.png", // 마커이미지의 주소입니다
-    imageSize = new kakao.maps.Size(45, 61), // 마커이미지의 크기입니다
-    imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-  // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-  var markerImage = new kakao.maps.MarkerImage(
-      imageSrc,
-      imageSize,
-      imageOption
-    ),
-    markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
-
-  // 마커를 생성합니다
-  var marker = new kakao.maps.Marker({
-    position: markerPosition,
-    image: markerImage, // 마커이미지 설정
-  });
-
-  // 마커가 지도 위에 표시되도록 설정합니다
-  marker.setMap(map);
-
-  // 커스텀 오버레이가 표시될 위치입니다
-  var position = new kakao.maps.LatLng(37.54699, 127.09598);
-
-  // 커스텀 오버레이를 생성합니다
-  var customOverlay = new kakao.maps.CustomOverlay({
-    map: map,
-    position: position,
-    yAnchor: 1,
-  });
-
-  //부트스트랩 슬라이더
-  var slider = document.getElementById("myRange");
-  var output = document.getElementById("distance-input");
-  output.innerText = `${slider.value}km`;
-
-  slider.oninput = function () {
-    output.innerHTML = `${this.value}km`;
-  };
-  //sotreList Event
-  const dialogCloseBtn = document.getElementById('dialog-close');
-  dialogCloseBtn.addEventListener('click',()=>{
-    document.getElementById('detail-dialog').classList.remove('on')
-  })
+  const dialogCloseBtn = document.getElementById("dialog-close");
+  const mobileToggle = document.getElementById("mobile-store-list");
   const storeList = document.getElementById("storeList");
-  storeList.addEventListener("click", (e) => {
-    let target = e.target;
-    if (target.classList.contains("like-btn")) {
-      target.style.backgroundColor = "pink";
-      return;
-    }
-    while (!target.classList.contains("store-card-container")) {
-      if (target === storeList) {
-        target = null;
+  const addressChangeBtn = document.getElementById('addressChangeBtn');
+  const addressBarBottom = document.getElementById('addressBarBottom');
+
+
+  const addEventHandler = (elem,type,callback) => {
+    elem.addEventListener(type,callback);
+  }
+  const addressChangeHandler = () => {
+    alert("주소변경")
+  }
+ 
+  
+
+  const createMap = () => {
+    var mapContainer = document.getElementById("store-map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
+        level: 4, // 지도의 확대 레벨
+      };
+
+    var map = new kakao.maps.Map(mapContainer, mapOption);
+
+    var imageSrc = "../../assets/img/marker-2.png", // 마커이미지의 주소입니다
+      imageSize = new kakao.maps.Size(45, 61), // 마커이미지의 크기입니다
+      imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      ),
+      markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+      image: markerImage, // 마커이미지 설정
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    // 커스텀 오버레이가 표시될 위치입니다
+    var position = new kakao.maps.LatLng(37.54699, 127.09598);
+
+    // 커스텀 오버레이를 생성합니다
+    var customOverlay = new kakao.maps.CustomOverlay({
+      map: map,
+      position: position,
+      yAnchor: 1,
+    });
+  };
+
+  const createSlider = () => {
+    //부트스트랩 슬라이더
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("distance-input");
+    output.innerText = `${slider.value}km`;
+
+    slider.oninput = function () {
+      output.innerHTML = `${this.value}km`;
+    };
+  };
+
+  const storeListEventHandler = () => {
+    dialogCloseBtn.addEventListener("click", () => {
+      document.getElementById("detail-dialog").classList.remove("on");
+    });
+
+    storeList.addEventListener("click", (e) => {
+      let target = e.target;
+      if (target.classList.contains("like-btn")) {
+        target.style.backgroundColor = "pink";
         return;
       }
-      target = target.parentNode;
-    }
-    
-    document.getElementById('detail-dialog').classList.add('on')
+      while (!target.classList.contains("store-card-container")) {
+        if (target === storeList) {
+          target = null;
+          return;
+        }
+        target = target.parentNode;
+      }
 
-    var roadviewContainer = document.getElementById("roadview"); //로드뷰를 표시할 div
-    var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
-    var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
+      document.getElementById("detail-dialog").classList.add("on");
 
-    var position = new kakao.maps.LatLng(33.450701, 126.570667);
+      var roadviewContainer = document.getElementById("roadview"); //로드뷰를 표시할 div
+      var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
+      var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
 
-    // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
-    roadviewClient.getNearestPanoId(position, 50, function (panoId) {
-      roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
+      var position = new kakao.maps.LatLng(33.450701, 126.570667);
+
+      // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
+      roadviewClient.getNearestPanoId(position, 50, function (panoId) {
+        roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
+      });
     });
-  });
-  //mobile-list-toggle
-  const mobileToggle = document.getElementById("mobile-store-list");
-  mobileToggle.addEventListener("click", () => {
-    document.body.classList.toggle("mobile-toggle");
+  };
 
-    
-  });
+  const mobileToggleHandler = () => {
+    mobileToggle.addEventListener("click", () => {
+      document.body.classList.toggle("mobile-toggle");
+    });
+  };
+
+  (function init() {
+    createMap();
+    createSlider();
+    storeListEventHandler();
+    mobileToggleHandler();
+  })();
+
+   
+  addEventHandler(addressChangeBtn,'click',addressChangeHandler);
+  addEventHandler(addressBarBottom,'click',addressChangeHandler);
+
+
+
 });
